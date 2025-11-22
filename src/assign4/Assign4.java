ef64069c64542665aa62133694a36feb8a0588eb
LynnethTigsePena4240 <lynneth_tigsepena@hotmail.com>
2025-11-22 13:25:05 -0500

    started with adding the GUI for the assignment (not done yet)

--- src/assign4/Assign4.java
+++ src/assign4/Assign4.java
@@ -6,15 +6,95 @@ package assign4;
 
 import java.io.*;
 import java.util.*;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 /**
  *
  * @author lynne
  */
-public class Assign4 {
+public class Assign4 extends Application{
 
     /**
      * @param args the command line arguments
      */
+    @Override
+    public void start(Stage stage){
+        ListView<String> list = new ListView<String>();
+        ObservableList<String> items = FXCollections.observableArrayList("hotel 1","hotel 2","hotel 3");
+        list.setItems(items);
+        
+        Button starBtn = new Button("Star Rating");
+        starBtn.setStyle("-fx-background-color: #add8e6;");
+         //when mouse hovering over btn
+        starBtn.setOnMouseEntered(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                starBtn.setStyle("-fx-background-color: #c9eaf5");
+            }
+        });
+        //when mouse no longer hovering over btn
+        starBtn.setOnMouseExited(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                starBtn.setStyle("-fx-background-color: #add8e6");
+            }
+        });
+        
+        Button lowestBtn = new Button("Lowest Price");
+        lowestBtn.setStyle("-fx-background-color: #add8e6;");
+        //when mouse is hovering over btn
+        lowestBtn.setOnMouseEntered(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                lowestBtn.setStyle("-fx-background-color: #c9eaf5");
+            }
+        });
+        //when mouse is no longer hovering over 
+        lowestBtn.setOnMouseExited(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                lowestBtn.setStyle("-fx-background-color: #add8e6");
+            }
+        });
+        
+        Button saveBtn = new Button("Save");
+        saveBtn.setStyle("-fx-background-color: #add8e6;");
+        
+        //when mouse is hovering over btn
+        saveBtn.setOnMouseEntered(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                saveBtn.setStyle("-fx-background-color: #c9eaf5");
+            }
+        });
+        //when mouse is no longer hovering over
+        saveBtn.setOnMouseExited(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                saveBtn.setStyle("-fx-background-color: #add8e6");
+            }
+        });
+        
+        HBox hbox = new HBox(10, starBtn, lowestBtn, saveBtn);
+        
+        VBox vbox = new VBox(15, list, hbox);
+        vbox.setPadding(new Insets(15, 12, 13, 14));
+        Scene scene = new Scene(vbox, 400, 320);
+
+        stage.setScene(scene);
+        stage.setTitle("Hotel List");
+        stage.show();
+    }
     
     static class Hotel implements Comparable<Hotel>{
         String name;
@@ -50,6 +130,7 @@ public class Assign4 {
     public static void main(String[] args) throws IOException{
         // TODO code application logic here
         //created a file object to make the file to read from
+        launch(args);
         File file1 = new File("src/assign4/Hotels.txt"); 
         List<Hotel> hotels = new ArrayList<>();
         //reading the file
